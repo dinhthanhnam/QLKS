d24e52daabc91b3446c005fc7845134844c58f45
dinhthanhnam <149922885+dinhthanhnam@users.noreply.github.com>
2023-11-22 23:11:44 +0700

    cập nhật view

--- views/ThongTinPhong.sql
+++ views/ThongTinPhong.sql
@@ -15,13 +15,3 @@ order by MAPHONG;
 
 select * FROM THONGTINPHONG;
 
---phòng còn trống tại thời điểm hiện tại (chưa hoàn thiện)
-CREATE OR REPLACE VIEW PhongTrongView AS
-SELECT *
-FROM ThongTinPhong
-WHERE maphong NOT IN (
-    SELECT maphong
-    FROM chitietphong ctp
-    JOIN datphong dp ON ctp.madatphong = dp.madatphong
-    WHERE dp.checkoutdate >= SYSDATE
-);
